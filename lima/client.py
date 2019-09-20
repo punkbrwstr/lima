8daae7c00a9938c482c6e9b1b17f56a191091f61
Peter Graf <magnumpi@gmail.com>
2019-09-20 11:56:44 -0400

    Removed prefixes from keys

--- lima/client.py
+++ lima/client.py
@@ -7,10 +7,6 @@ from redis.connection import UnixDomainSocketConnection
 import pynto.time as time
 from pynto.main import _Word,Column
 
-SERIES_PREFIX = 'l.s'
-FRAME_PREFIX = 'l.f'
-HASH_PREFIX = 'l.h'
-
 Type = namedtuple('Type', ['code', 'pad_value', 'length'])
 
 TYPES = {
@@ -40,7 +36,7 @@ class Lima(object):
     def _get_redis(self):
         return redis.Redis(connection_pool=self._pool)
 
-    def _read_metadata(self, key):
+    def read_metadata(self, key):
         data = self._get_redis().getrange(key,0,METADATA_SIZE-1)
         if len(data) == 0:
             return None
@@ -94,11 +90,10 @@ class Lima(object):
     def _list_keys(self, match='*'):
         return [key for key in self._get_redis().scan_iter(match=match)]
 
-    def read_series_data(self, key, start=None, end=None, periodicity=None, resample_method='last'):
-        series_key = f'{SERIES_PREFIX}.{key}'
-        md = self._read_metadata(series_key)
+    def read_series_data(self, series_key, start=None, end=None, periodicity=None, resample_method='last'):
+        md = self.read_metadata(series_key)
         if md is None:
-            raise KeyError(f'No series data for key: "{key}".')
+            raise KeyError(f'No series data for key: "{series_key}".')
         needs_resample = not (periodicity is None or periodicity == md.periodicity)
         if needs_resample:
             if not start is None:
@@ -138,10 +133,9 @@ class Lima(object):
         data = self.read_series_data(key, start, end, periodicity, resample_method)
         return pd.Series(data[3], index=time.get_datetimeindex(data[2],data[0],data[1]), name=key)
 
-    def write_series(self, key, series):
-        series_key = f'{SERIES_PREFIX}.{key}'
+    def write_series(self, series_key, series):
         series_md = self._get_metadata_for_series(series)
-        saved_md = self._read_metadata(series_key)
+        saved_md = self.read_metadata(series_key)
         if saved_md and saved_md.periodicity != series_md.periodicity:
             raise Exception(f'Incompatible periodicity.')   
         data = series.values
@@ -160,39 +154,29 @@ class Lima(object):
             self._update_end(series_key, series_md.end)
 
     def delete_series(self, key):
-        self._delete(f'{SERIES_PREFIX}.{key}')
+        self._delete(key)
 
-    def read_series_metadata(self, key, date_range=None):
-        series_key = f'{SERIES_PREFIX}.{key}'
-        return self._read_metadata(series_key)
-
-    def truncate_series(self, key, end_date):
-        series_key = f'{SERIES_PREFIX}.{key}'
-        md = self._read_metadata(series_key)
+    def truncate_series(self, series_key, end_date):
+        md = self.read_metadata(series_key)
         end_index = get_index(md.periodicity, end_date)
         if end_index < md.end:
             self._update_end(series_key, end_index)
 
-    def list_series(self, match='*'):
-        return [key.decode().replace(SERIES_PREFIX + '.','') for key in self._list_keys(f'{SERIES_PREFIX}.{match}')]
-
-    def read_frame_headers(self, key):
-        frame_key = f'{FRAME_PREFIX}.{key}'
+    def read_frame_headers(self, frame_key):
         return self._get_data_range(frame_key, 0, -1).decode().split('\t')
 
     def read_frame_series_keys(self, key):
         return [f'{key}.{c}' for c in self.read_frame_headers(key)]
 
-    def read_frame_data(self, key, start=None, end=None, periodicity=None, resample_method='last'):
-        frame_key = f'{FRAME_PREFIX}.{key}'
-        md = self._read_metadata(frame_key)
+    def read_frame_data(self, frame_key, start=None, end=None, periodicity=None, resample_method='last'):
+        md = self.read_metadata(frame_key)
         if md is None:
-            raise KeyError(f'No frame data for key: "{key}".')
+            raise KeyError(f'No frame data for key: "{frame_key}".')
         start = md.start if start is None else get_index(md.periodicity, start)
         end = md.end if end is None else get_index(md.periodicity, end)
         periodicity = md.periodicity if periodicity is None else periodicity
-        columns = self.read_frame_headers(key)
-        data = np.column_stack([self.read_series_data(f'{key}.{c}', start, end, periodicity,
+        columns = self.read_frame_headers(frame_key)
+        data = np.column_stack([self.read_series_data(f'{frame_key}.{c}', start, end, periodicity,
                     resample_method)[3] for c in columns])
         return (start, end, periodicity, columns, data)
 
@@ -201,9 +185,8 @@ class Lima(object):
         return pd.DataFrame(data[4], columns=data[3],
                     index=time.get_datetimeindex(data[2], data[0], data[1]))
 
-    def write_frame(self, key, frame):
-        frame_key = f'{FRAME_PREFIX}.{key}'
-        md = self._read_metadata(frame_key)
+    def write_frame(self, frame_key, frame):
+        md = self.read_metadata(frame_key)
         end = time.get_index(frame.index.freq.name, frame.index[-1].date()) + 1
         if md is None:
             start = time.get_index(frame.index.freq.name, frame.index[0].date())
@@ -219,7 +202,7 @@ class Lima(object):
             first_save = False
         new_columns = []
         for column,series in frame.iteritems():
-            series_code = f'{key}.{column}'
+            series_code = f'{frame_key}.{column}'
             if not column in columns:
                 columns.add(column)
                 new_columns.append(column)
@@ -230,43 +213,35 @@ class Lima(object):
         elif len(new_columns) > 0:
             self._append(frame_key, ('\t' + '\t'.join(new_columns)).encode()) 
 
-    def delete_frame(self, key):
-        for series in self.read_frame_series_keys(key):
+    def delete_frame(self, frame_key):
+        for series in self.read_frame_series_keys(frame_key):
             self.delete_series(series)
-        self._delete(f'{FRAME_PREFIX}.{key}')
+        self._delete(frame_key)
 
-    def read_frame_metadata(self, key):
-        frame_key = f'{FRAME_PREFIX}.{key}'
-        return self._read_metadata(frame_key)
-
-    def truncate_frame(self, key, end_date):
-        frame_key = f'{FRAME_PREFIX}.{key}'
-        md = self._read_metadata(frame_key)
+    def truncate_frame(self, frame_key, end_date):
+        md = self.read_metadata(frame_key)
         end_index = time.get_index(md.periodicity, end_date)
         if end_index < md.end:
             self._update_end(frame_key, end_index)
 
-    def list_frames(self, match='*'):
-        return [key.decode().replace(FRAME_PREFIX + '.','') for key in self._list_keys(f'{FRAME_PREFIX}.{match}')]
-
     def read_hash_item(self, key, item):
-        values = self._hash_get(f'{HASH_PREFIX}.{key}', item) 
+        values = self._hash_get(key, item) 
         if not isinstance(item,list):
             values = [values]
         return [v.decode() if not v is None else None for v in values ]
 
     def read_hash(self, key):
-        values = self._hash_get(f'{HASH_PREFIX}.{key}')
+        values = self._hash_get(key)
         return {k.decode(): v.decode() for k,v in values.items()} if len(values) > 0 else None
         
     def write_hash(self, key, items):
-        self._hash_set(f'{HASH_PREFIX}.{key}', items)
+        self._hash_set(key, items)
 
     def write_hash_item(self, key, item, value):
-        self._hash_set(f'{HASH_PREFIX}.{key}', item, value)
+        self._hash_set(key, item, value)
 
     def delete_hash(self, key):
-        self._delete(f'{HASH_PREFIX}.{key}')
+        self._delete(key)
 
     def series_col(self, series_key):
         return _PyntoSeries(self, 'lima_col')(series_key)
