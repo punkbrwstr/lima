3e062851cefcfa5ffe1a04107708e89250d39857
Peter Graf <magnumpi@gmail.com>
2020-08-02 13:40:18 -0400

    Fixed package dependencies

--- setup.py
+++ setup.py
@@ -13,6 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/punkbrwstr/lima",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'redis',
+        'hiredis'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
